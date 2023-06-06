0c97f9c92b7e3655be99d3ed13409d473091a94a
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-06-06 17:23:32 +0200

    add palindrome

--- js/palindrome.js
+++ js/palindrome.js
@@ -2,6 +2,8 @@ console.log('JS ok')
 
 /*****  es.1  ******/
 /*
+Palindroma
+
 - Chiedere all’utente di inserire una parola
 - Creare una funzione per capire se la parola inserita è palindroma
 */
@@ -10,3 +12,21 @@ console.log('JS ok')
 const inputWord = prompt('Inserisci una parola').trim();
 
 console.log(inputWord)
+
+let lunghezza = inputWord.length
+
+for (i = 0; i < lunghezza; i++){
+
+    //A B B A 
+    //0 1 2 3
+
+    // O V A T T A V O
+    // 0 1 2 3 4 5 6 7 
+
+    if (inputWord[i] == inputWord[lunghezza - 1]) {
+        console.log('palindroma')
+    } else {
+    }
+
+    console.log(i)
+}
